8411963c13cc20294c7983fd5b13cdf01bf7366d
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-09 12:02:05 +0200

    funciona

--- config/express.js
+++ config/express.js
@@ -7,7 +7,7 @@ const path         = require('path');
 
 module.exports = (app) => {
   // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
+  app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'ejs');
 
   app.use(logger('dev'));
